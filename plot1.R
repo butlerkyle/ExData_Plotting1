c22b6c04a74f8847b7a493df003e79b443fc9a4a
Kyle Butler <kyle525@gmail.com>
2015-04-11 10:13:46 -0400

    Adding files

---
+++ plot1.R
@@ -0,0 +1,25 @@
+plot1 <- function(filename, sep = ";"){
+        ## read in the text file
+        data <- read.table(filename, sep = sep, header = TRUE)
+        
+        ## convert date for subseting
+        data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+        
+        ##subset to include only dates 2007-02-01 and 2007-02-02
+        date1 <- as.Date('01/02/2007', '%d/%m/%Y')
+        date2 <- as.Date('02/02/2007', '%d/%m/%Y')
+        data <- data[data$Date >= date1 & data$Date <= date2,]
+        
+        #$ convert time and merge datetime variable
+        data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
+        data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+        
+        ## create the histogram
+        hist(data$Global_active_power, main = "Global Active Power", 
+             col = "red", xlab = "Global Active Power (kilowatts)")
+        
+        ##copy a png file to the working directory
+        dev.copy(png, file = "plot1.png", width = 480, height = 480)
+        dev.off()
+        
+}
---
+++ plot3.r
@@ -0,0 +1,33 @@
+plot3 <- function(filename, sep = ";"){
+        ## read in the text file
+        data <- read.table(filename, sep = sep, header = TRUE)
+        
+        ## convert date for subseting
+        data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+        
+        ##subset to include only dates 2007-02-01 and 2007-02-02
+        date1 <- as.Date('01/02/2007', '%d/%m/%Y')
+        date2 <- as.Date('02/02/2007', '%d/%m/%Y')
+        data <- data[data$Date >= date1 & data$Date <= date2,]
+        
+        #$ convert time and merge datetime variable
+        data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
+        data$Global_active_power <- as.numeric(as.character(data$Global_active_power))
+        
+        ##convert submetering to numeric
+        data$Sub_metering_1 <- as.numeric(as.character(data$Sub_metering_1))
+        data$Sub_metering_2 <- as.numeric(as.character(data$Sub_metering_2))
+        data$Sub_metering_3 <- as.numeric(as.character(data$Sub_metering_3))
+        
+        ## create the plot
+        plot(data$DateTime, data$Sub_metering_1, type = "l", 
+             ylab = "Energy sub metering", xlab = "")
+        points(data$DateTime, data$Sub_metering_2, type = "l", col = "red")
+        points(data$DateTime, data$Sub_metering_3, type = "l", col = "blue")
+        legend('topright', names(data)[7:9], col = c("black", "red", "blue"), lty = 1, cex = 0.75)
+        
+        ##copy a png file to the working directory
+        dev.copy(png, file = "plot3.png", width = 480, height = 480)
+        dev.off()
+        
+}
